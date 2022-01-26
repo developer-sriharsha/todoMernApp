dccc36c2caa20f64dc73d7883437e6cdb34dcef2
harsha <sriharsha.katragadda@devrabbit.com>
2022-01-26 10:21:10 -0800

    s

--- src/components/VidCon.jsx
+++ src/components/VidCon.jsx
@@ -5,8 +5,6 @@ import socketIOClient from "socket.io-client";
 console.log("lksdjflkdfj",socketIOClient)
 const socket = socketIOClient('https://ayur-care.ayurway.com:8050');
 const VidCon = ({ children }) => {
-
-
     const [idToCall, setIdToCall] = useState('');
     const connectionRef = useRef();
     const [callAccepted, setCallAccepted] = useState(false);
@@ -20,7 +18,11 @@ const VidCon = ({ children }) => {
     const userVideo = useRef();
 
     useEffect(async () => {
-
+        socket.on('me', (id) => {
+                console.log("new sssssssssssssid-",id)
+                setMe(id)
+            }
+        )
         await  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((currentStream) => {
                 setStream(currentStream);
@@ -28,12 +30,7 @@ const VidCon = ({ children }) => {
                 myVideo.current.srcObject = currentStream;
             });
 
-        socket.on('me', (id) => {
-                console.log("new id-",id)
-                setMe(id)
-            }
 
-        )
 
         socket.on('callUser', ({ from, name: callerName, signal }) => {
             setCall({ isReceivingCall: true, from, name: callerName, signal });
@@ -90,6 +87,32 @@ const VidCon = ({ children }) => {
 
         window.location.reload();
     };
+    const handleStopStream=()=>{
+        const myTracks = stream.getTracks();
+        const myAudio = myTracks.filter(track => track.kind === "audio")[0];
+        const myVideo = myTracks.filter(track => track.kind === "video")[0];
+        myAudio.enabled = false;
+    }
+    const handleEnableStream=()=>{
+        const myTracks = stream.getTracks();
+        const myAudio = myTracks.filter(track => track.kind === "audio")[0];
+        const myVideo = myTracks.filter(track => track.kind === "video")[0];
+        myAudio.enabled = true;
+    }
+
+    const handleStopStreamv=()=>{
+        const myTracks = stream.getTracks();
+        const myAudio = myTracks.filter(track => track.kind === "audio")[0];
+        const myVideo = myTracks.filter(track => track.kind === "video")[0];
+        myVideo.enabled = false;
+    }
+    const handleEnableStreamv=()=>{
+        const myTracks = stream.getTracks();
+        const myAudio = myTracks.filter(track => track.kind === "audio")[0];
+        const myVideo = myTracks.filter(track => track.kind === "video")[0];
+        myVideo.enabled = true;
+    }
+
     console.log("fkjldsfjksldjfdfj",me)
     return (
         <div >
@@ -110,6 +133,13 @@ const VidCon = ({ children }) => {
                         </div>
                     </div>
                 )}
+                <button className="btn btn-primary" type="button" onClick={handleStopStream}>stop Audio</button>
+                <button className="btn btn-primary" type="button" onClick={handleEnableStream}>On Audio</button>
+
+                <button className="btn btn-primary" type="button" onClick={handleStopStreamv}>stop Video</button>
+                <button className="btn btn-primary" type="button" onClick={handleEnableStreamv}>On Video</button>
+
+
                 {call.isReceivingCall && !callAccepted && (
                     <div style={{ display: 'flex', justifyContent: 'space-around' }}>
                         <h1>{call.name} is calling:</h1>
@@ -118,30 +148,30 @@ const VidCon = ({ children }) => {
                         </button>
                     </div>
                 )}
-                    <div  className="col-md-5">
-                        <div  >
-                            <h6 >Account Info</h6>
-                            <input className="form-control name-input" type="name" label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-                            <CopyToClipboard text={me} >
-                                <button  className="btn btn-primary" variant="contained" color="primary" >
-                                    Copy Your ID
-                                </button>
-                            </CopyToClipboard>
-                        </div>
-                        <div >
-                            <h6 >Make a call</h6>
-                            <input className="form-control name-input" type="name" label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
-                            {callAccepted && !callEnded ? (
-                                <button  className="btn btn-primary" variant="contained" color="secondary"  onClick={leaveCall} >
-                                    Hang Up
-                                </button>
-                            ) : (
-                                <button  className="btn btn-primary" variant="contained" color="primary" onClick={(e) => callUser(e,idToCall)} >
-                                    Call
-                                </button>
-                            )}
-                        </div>
+                <div  className="col-md-5">
+                    <div  >
+                        <h6 >Account Info</h6>
+                        <input className="form-control name-input" type="name" label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+                        <CopyToClipboard text={me} >
+                            <button  className="btn btn-primary" variant="contained" color="primary" >
+                                Copy Your ID
+                            </button>
+                        </CopyToClipboard>
                     </div>
+                    <div >
+                        <h6 >Make a call</h6>
+                        <input className="form-control name-input" type="name" label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+                        {callAccepted && !callEnded ? (
+                            <button  className="btn btn-primary" variant="contained" color="secondary"  onClick={leaveCall} >
+                                Hang Up
+                            </button>
+                        ) : (
+                            <button  className="btn btn-primary" variant="contained" color="primary" onClick={(e) => callUser(e,idToCall)} >
+                                Call
+                            </button>
+                        )}
+                    </div>
+                </div>
 
                 {children}
             </div>
